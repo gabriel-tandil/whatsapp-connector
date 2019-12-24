87ce8ccecadafc668a89b7d9297a07aac334dd75
Gabriel Alvarez <gabriel.tandil@gmail.com>
2019-12-24 09:12:21 -0300

    aun no hace nada nuevo, es el ejemplo de la biblioteca

---
+++ whatsapp-connector.js
@@ -0,0 +1,138 @@
+var fs = require('fs');
+const {Client} = require("whatsapp-web.js");
+
+if (fs.existsSync('./config.json')) {
+  var sessionCfg = require('./config.json');
+}
+
+const client = new Client({puppeteer: {headless: false
+ , args: [
+        '--log-level=3', // fatal only
+        '--start-maximized',
+        '--no-default-browser-check',
+        '--disable-infobars',
+        '--disable-web-security',
+        '--disable-site-isolation-trials',
+        '--no-experiments',
+        '--ignore-gpu-blacklist',
+        '--ignore-certificate-errors',
+        '--ignore-certificate-errors-spki-list',
+        '--disable-gpu',
+        '--disable-extensions',
+        '--disable-default-apps',
+        '--enable-features=NetworkService',
+        '--disable-setuid-sandbox',
+        '--no-sandbox'
+      ]
+} ,session:sessionCfg});
+// You can use an existing session and avoid scanning a QR code by adding a "session" object to the client options.
+// This object must include WABrowserId, WASecretBundle, WAToken1 and WAToken2.
+
+client.initialize();
+
+client.on('qr', (qr) => {
+    // NOTE: This event will not be fired if a session is specified.
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('authenticated', (session) => {
+    console.log('AUTHENTICATED', session);
+    if (!fs.existsSync('./config.json')) {
+        fs.writeFile("./config.json", JSON.stringify(session), function(err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+
+    }
+    
+});
+
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessfull
+    console.error('AUTHENTICATION FAILURE', msg);
+})
+
+client.on('ready', () => {
+    console.log('READY');
+});
+
+client.on('message', async msg => {
+    console.log('MESSAGE RECEIVED', msg);
+
+    if (msg.body == '!ping reply') {
+        // Send a new message as a reply to the current one
+        msg.reply('pong');
+
+    } else if (msg.body == '!ping') {
+        // Send a new message to the same chat
+        client.sendMessage(msg.from, 'pong');
+
+    } else if (msg.body.startsWith('!subject ')) {
+        // Change the group subject
+        let chat = await msg.getChat();
+        if(chat.isGroup) {
+            let newSubject = msg.body.slice(9);
+            chat.setSubject(newSubject);
+        } else {
+            msg.reply('This command can only be used in a group!');
+        }
+    } else if (msg.body.startsWith('!echo ')) {
+        // Replies with the same message
+        msg.reply(msg.body.slice(6));
+    } else if (msg.body.startsWith('!desc ')) {
+        // Change the group description
+        let chat = await msg.getChat();
+        if(chat.isGroup) {
+            let newDescription = msg.body.slice(6);
+            chat.setDescription(newDescription);
+        } else {
+            msg.reply('This command can only be used in a group!');
+        }
+    } else if (msg.body == '!leave') {
+        // Leave the group
+        let chat = await msg.getChat();
+        if(chat.isGroup) {
+            chat.leave();
+        } else {
+            msg.reply('This command can only be used in a group!');
+        }
+    } else if(msg.body == '!groupinfo') {
+        let chat = await msg.getChat();
+        if(chat.isGroup) {
+            msg.reply(`
+                *Group Details*
+                Name: ${chat.name}
+                Description: ${chat.description}
+                Created At: ${chat.createdAt.toString()}
+                Created By: ${chat.owner.user}
+                Participant count: ${chat.participants.length}
+            `);
+        } else {
+            msg.reply('This command can only be used in a group!');
+        }
+    } else if(msg.body == '!chats') {
+        const chats = await client.getChats();
+        client.sendMessage(msg.from, `The bot has ${chats.length} chats open.`);
+    } else if(msg.body == '!mediainfo' && msg.hasMedia) {
+        const attachmentData = await msg.downloadMedia();
+        msg.reply(`
+            *Media info*
+            MimeType: ${attachmentData.mimetype}
+            Filename: ${attachmentData.filename}
+            Data (length): ${attachmentData.data.length}
+        `);
+    }
+});
+
+client.on('message_create', (msg) => {
+    // Fired on all message creations, including your own
+    if(msg.fromMe) {
+        // do stuff here
+    }
+})
+
+client.on('disconnected', () => {
+    console.log('Client was logged out');
+})
+
